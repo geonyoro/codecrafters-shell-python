bed29de30581e1ae8f730ed90319f8753bc69450
George Nyoro <geonyoro@gmail.com>
2025-05-08 20:33:06 +0300

    exit command

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 
@@ -6,6 +7,9 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         command = input()
+        mobj = re.match(r"exit (\d+)", command)
+        if mobj:
+            sys.exit(int(mobj[1]))
         print(f"{command}: command not found")
 
 
