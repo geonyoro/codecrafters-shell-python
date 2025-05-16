bb279528e9dbc76b68cc48e81efb719dd7e09b29
George Nyoro <geonyoro@gmail.com>
2025-05-16 13:10:11 +0300

    feat: Change test suite functionality

--- test.py
+++ test.py
@@ -2,23 +2,61 @@ import unittest
 
 from app import main
 
+# line 1 is the input, line 2 is the output
+
 
 class Tests(unittest.TestCase):
     def test_cmd_cleaner(self):
-        for input, output in {
-            "echo hello world": "echo hello world",
-            "echo hello   world": "echo hello world",
-            "echo 'shell hello'": "echo shell hello",
-            "echo 'world     test'": "echo world     test",
-            'echo "quz  hello"  "bar"': "echo quz  hello bar",
-            'echo "bar"  "shell\'s"  "foo"': "echo bar shell's foo",
-            r'echo "before\   after"': r"echo before\   after",
-            r"echo world\ \ \ \ \ \ script": "echo world      script",
-            r"echo \\x": r"echo \x",
-            r"echo shell\ \ \ \ \ \ hello": "echo shell      hello",
-            # meant to reflect echo \'\"shell example\"\'
-            "echo \\'\\\"shell example\\\"\\'": "echo '\"shell example\"'",
-        }.items():
+        test_cases = r"""
+in:echo hello   world
+out:echo hello world
+
+in:echo 'shell hello'
+out:echo shell hello
+
+in:echo 'world     test'
+out:echo world     test
+
+in:echo "quz  hello"  "bar"
+out:echo quz  hello bar
+
+in:echo "bar"  "shell's"  "foo"
+out:echo bar shell's foo
+
+in:echo "before\   after"
+out:echo before\   after
+
+in:echo world\ \ \ \ \ \ script
+out:echo world      script
+
+in:echo \\x
+out:echo \x
+
+in:echo shell\ \ \ \ \ \ hello
+out:echo shell      hello
+
+in:echo \'\"shell example\"\'
+out:echo '"shell example"'
+
+in:"/tmp/bar/f\n39" "/tmp/bar/f\64" "/tmp/bar/f'\'56"
+out:echo /tmp/bar/f\n39 /tmp/bar/f\64 /tmp/bar/f'\'56
+"""
+        ins = []
+        outs = []
+        for line in test_cases.split("\n"):
+            line = line.strip()
+            if not line:
+                continue
+            verb, _, cmd = line.partition(":")
+            if verb not in ["in", "out"]:
+                raise ValueError(f"Invalid {verb=}")
+            if verb == "in":
+                ins.append(cmd)
+            else:
+                outs.append(cmd)
+
+        assert len(ins) == len(outs)
+        for input, output in zip(ins, outs):
             with self.subTest(input=input, output=output):
                 toutput = main.cmd_cleaner(input)
                 self.assertEqual(output, toutput)
