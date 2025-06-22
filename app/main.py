b2b4e521816bc08e8b9c6eaa8cc63eb2c63761cb
George Nyoro <geonyoro@gmail.com>
2025-06-22 19:33:28 +0300

    fix: Change ending

--- app/main.py
+++ app/main.py
@@ -84,7 +84,7 @@ def completer_func(text, state):
             if not has_printed_bell:
                 has_printed_bell = True
                 sys.stdout.write("\a")
-                print(f"$ {text}", end="")
+                print(f"$ {text}")
             else:
                 print("  ".join(matches))
                 print("$ ", end="")
